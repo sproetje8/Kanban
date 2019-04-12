c344f25e4f952255586597723662f08de66ba672
Evelien Kulikova <evelienfoubert@mail.ru>
2019-04-12 11:43:47 +0300

    create board on click

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-function init() {
-    window.localStorage.setItem('div', 'new board');
-}
+document.getElementById("create-board").addEventListener("click", function(){
+    var newboard = document.createElement("li");
+    var boarddiv = document.createElement("div");
+    newboard.className = "board-tile";
+    boarddiv.className = "created-board board-tile-new";
+    boarddiv.innerHTML = "I am a new board";
+    newboard.appendChild(boarddiv);
+    document.getElementById("boardList").appendChild(newboard);
+});
+
 
