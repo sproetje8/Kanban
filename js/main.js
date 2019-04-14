e779625b47ade708b2c072cf8210b7ac557eed97
Evelien Kulikova <evelienfoubert@mail.ru>
2019-04-14 11:25:54 +0300

    rename main.html to index.html and add modal popup

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-document.getElementById("create-board").addEventListener("click", function(){
-    var newboard = document.createElement("li");
-    var boarddiv = document.createElement("div");
-    newboard.className = "board-tile";
-    boarddiv.className = "created-board board-tile-new";
-    boarddiv.innerHTML = "I am a new board";
-    newboard.appendChild(boarddiv);
-    document.getElementById("boardList").appendChild(newboard);
+document.getElementById('create-board').addEventListener('click', function(){
+document.querySelector('.bg-modal').style.display = 'flex';
 });
 
+document.querySelector('.close').addEventListener('click', function(){
+    document.querySelector('.bg-modal').style.display = 'none';
+});
+
+var boardname = document.getElementById('boardName').value;
+document.getElementById('create-new-board').addEventListener('click', function(){
+    var boarddiv = document.createElement('div');
+    boarddiv.className = 'created-board board-tile-new';
+    boarddiv.innerHTML = boardname;
+    
+    var newboard = document.createElement('li');
+    newboard.className = 'board-tile';
+    newboard.appendChild(boarddiv);
+    document.getElementById('boardList').appendChild(newboard);
+    document.querySelector('.bg-modal').style.display = 'none';
+});
+
+// document.getElementById('create-board').addEventListener('click', function(){
+    
+// });
 
