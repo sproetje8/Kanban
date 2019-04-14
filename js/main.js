e750b8d5d02287ee110529faad8518ad1494e5b1
Evelien Kulikova <evelienfoubert@mail.ru>
2019-04-14 12:29:47 +0300

    add new boards with board names

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ document.querySelector('.close').addEventListener('click', function(){
     document.querySelector('.bg-modal').style.display = 'none';
 });
 
-var boardname = document.getElementById('boardName').value;
 document.getElementById('create-new-board').addEventListener('click', function(){
+    var boardname = document.getElementById('boardName').value;
+
     var boarddiv = document.createElement('div');
     boarddiv.className = 'created-board board-tile-new';
     boarddiv.innerHTML = boardname;
@@ -15,11 +16,10 @@ document.getElementById('create-new-board').addEventListener('click', function()
     var newboard = document.createElement('li');
     newboard.className = 'board-tile';
     newboard.appendChild(boarddiv);
+    
     document.getElementById('boardList').appendChild(newboard);
+
     document.querySelector('.bg-modal').style.display = 'none';
 });
 
-// document.getElementById('create-board').addEventListener('click', function(){
-    
-// });
 
