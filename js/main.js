6fa3af36ba00c47898698689413de7c85c99c947
Evelien Kulikova <evelienfoubert@mail.ru>
2019-04-18 08:44:53 +0300

    add button to delete

--- js/main.js
+++ js/main.js
@@ -6,76 +6,80 @@ document.querySelector('.close').addEventListener('click', function(){
     document.querySelector('.bg-modal').style.display = 'none';
 });
 
-document.getElementById('create-new-board').addEventListener('click', function(){
-    // var obj = {};
-    var boardname = document.getElementById('boardName').value;
-    obj.name = boardname;
+var newboard;
+var boardname;
 
+document.getElementById('create-new-board').addEventListener('click', function(){
+    boardname = document.getElementById('boardName').value;
+    
     var boarddiv = document.createElement('div');
     boarddiv.className = 'created-board board-tile-new';
-        
+    
     // create link to boardpage
     var boardlink = document.createElement('a');
     boardlink.className = 'brdlnk';
     boardlink.id = 'brdId';
     boardlink.innerHTML = boardname;
     boardlink.setAttribute('href', 'www.google.com');
-
+    
     // function for layout of current Date
     var today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth()+1; 
     var yyyy = today.getFullYear();
-        if(dd<10) {
+    if(dd<10) {
         dd = '0'+ dd;
-        } 
-        if(mm<10) {
-            mm ='0'+ mm;
-        } 
+    } 
+    if(mm<10) {
+        mm ='0'+ mm;
+    } 
     today = mm + '-' + dd + '-' + yyyy;
     
     var createdOn = document.createElement('span');
     createdOn.className = 'dateCreated';
     createdOn.innerHTML = 'Created : ' + today;
     
-    // var lastEdit = document.createElement('span');
-    // lastEdit.className = 'dateLastEdit';  
-    // lastEdit.innerHTML = 'Last edit : ' + today;  
-
-    var delbtn = document.createElement('img');
-    delbtn.className = 'delete';
-    delbtn.id = 'del-board';
-    delbtn.setAttribute('src', '/images/delete-icon.png');
-    delbtn.setAttribute('alt', 'delete button');
-
     var settingsbtn = document.createElement('img');
     settingsbtn.className = 'settings';
     settingsbtn.id = 'board-settings';
     settingsbtn.setAttribute('src', '/images/settings-icon.png');
     settingsbtn.setAttribute('alt', 'settings button');
-
+    
+    var delimg = document.createElement('img');
+    delimg.className = 'del-img';
+    delimg.id = 'del-board-img';
+    delimg.setAttribute('src', '/images/delete-icon.png');
+    delimg.setAttribute('alt', 'delete button');
+    
+    var delbtn = document.createElement('button');
+    delbtn.className = 'delete';
+    delbtn.id = 'del-board';
+    delbtn.appendChild(delimg);
+    
     boarddiv.appendChild(boardlink);
-    boarddiv.appendChild(delbtn);
     boarddiv.appendChild(settingsbtn);
     boarddiv.appendChild(createdOn);
-    // boarddiv.appendChild(lastEdit);
-
-
-    var newboard = document.createElement('li');
+    boarddiv.appendChild(delbtn);
+    
+    newboard = document.createElement('li');
     newboard.className = 'board-tile';
     newboard.appendChild(boarddiv);
     
     document.getElementById('boardList').appendChild(newboard);
-
     
     document.querySelector('.bg-modal').style.display = 'none';
     
-    // var board = new Object;
-    // board.name = boardname;
-    // board.created = today;
+    return newboard;
+    return boardname;
+});
+document.getElementById('del-board').addEventListener('click', function(){
+    document.getElementById('boardList').removeChild(newboard);
+    return newboard;
 });
 
-// to be added later when clicking on 'Save button' in the boardpage
 // var lastEdit = document.createElement('span');
 // lastEdit.className = 'dateLastEdit';  
 // lastEdit.innerHTML = 'Last edit : ' + today;  
+// boarddiv.appendChild(lastEdit);
+// obj.name = boardname;
+// var obj = {};
