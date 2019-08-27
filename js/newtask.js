ec0e3010765d3fa956d0d0dfb0b6425553484047
Evelien Kulikova <evelienfoubert@mail.ru>
2019-08-27 23:31:22 +0300

    get data from new task page and store in local storage

--- js/newtask.js
+++ js/newtask.js
@@ -22,8 +22,6 @@ function addBoardsToDropDown (boards) {
     })
 }
 
-localStorage.setItem('tasks', JSON.stringify(tasks));
-
 document.getElementById('create-new-task').addEventListener('click', validateInput);
 
 function validateInput () {
@@ -31,6 +29,8 @@ function validateInput () {
     var descriptionElement = document.getElementById('description');
     var priorityElement = document.getElementById('priority');
     var notesElement = document.getElementById('notes');
+    var taskOwnerNameElement = document.getElementById('task-owner-name');
+    var taskOwnerLastNameElement = document.getElementById('task-owner-lastName');
     var typeElement = document.getElementById('task-type');
     var boardElement = document.getElementById('board');
 
@@ -38,41 +38,85 @@ function validateInput () {
     var description = descriptionElement.value;
     var priority = priorityElement.value;
     var notes = notesElement.value;
+    var taskOwnerName = taskOwnerNameElement.value;
     var type = typeElement.value;
     var board = boardElement.value;
 
         if (taskName === '' || taskName === ' ') {
             taskNameElement.className = 'invalid';
             alert('Task name is required');
+        } else {
+            taskNameElement.className = 'valid';
         }
 
         if (description === '' || description === ' ') {
             descriptionElement.className = 'invalid';
             alert('Task description is required');
+        } else {
+            descriptionElement.className = 'valid';
         }
 
         if (priority === '' || priority === ' ') {
             priorityElement.className = 'invalid';
+            alert('Priority is required');
+        } else {
+            priorityElement.className = 'valid';
         }
 
         if (notes === '' || notes === ' ') {
             notesElement.className = 'invalid';
+            alert('Notes are required');
+        } else {
+            notesElement.className = 'valid';
         }
 
         if (type === '' || type === ' ') {
             typeElement.className = 'invalid';
+            alert('The type is required');
+        } else {
+            typeElement.className = 'valid';
         }
 
         if (board === '' || board === ' ') {
             boardElement.className = 'invalid';
+            alert('The board is required');
+        } else {
+            boardElement.className = 'valid';
         }
+
+        if (taskNameElement.className === 'valid' &&
+            descriptionElement.className === 'valid' &&
+            priorityElement.className === 'valid' &&
+            notesElement.className === 'valid' &&
+            typeElement.className === 'valid' &&
+            boardElement.className === 'valid') {
+            taskObjectConstructor(taskName, description, priority, notes, taskOwnerName, taskOwnerLastNameElement, type, board);
+        } else {
+            document.getElementById('create-new-task').addEventListener('click', validateInput);
+        }
+
+}
+
+function taskObjectConstructor(taskName, description, priority, notes, taskOwnerName, taskOwnerLastNameElement, type, board) {
+    var taskObject = {
+        taskName : taskName,
+        description : description,
+        priority : priority,
+        notes : notes,
+        taskOwnerName : taskOwnerName,
+        taskOwnerLastNameElement : taskOwnerLastNameElement,
+        type : type,
+        board : board
+    };
+
+    tasks.push(taskObject);
+    addTaskToLocalStorage(tasks);
+}
+
+function addTaskToLocalStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    document.getElementById('frm-new-task').reset();
+    window.location.href = 'index.html';
 }
 
 
-
-// taskObjectConstructor();
-//
-// function taskObjectConstructor {
-//     taskObject =
-//
-// }
