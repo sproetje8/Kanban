f9f408dd56c48f6f00105a75d1a5015f56d65035
Evelien Kulikova <evelienfoubert@mail.ru>
2019-09-01 22:20:13 +0300

    add tasks to task owner row

--- js/board.js
+++ js/board.js
@@ -6,7 +6,7 @@ var selectedBoardName = selectedBoard.name;
 var filteredTasks = [];
 var uniqueTaskOwners = [];
 var firstClass;
-var toDO = 'toDo';
+var toDo = 'toDo';
 var inProgress = 'inProgress';
 var done = 'done';
 
@@ -27,6 +27,7 @@ function init(selectedBoardName, tasks) {
 
 getTaskOwnerInfo();
 showTaskOwners();
+createHtmlForTasks();
 
 function getTaskOwnerInfo() {
     var taskOwnerInfo = filteredTasks.map(function(filteredTask) {
@@ -43,7 +44,7 @@ function showTaskOwners () {
         firstClass = 'js-row-' + (i + 2);
         var secondClass = 'taskOwner';
         li.classList = firstClass + ' ' + secondClass;
-        li.id = 'js-' + uniqueTaskOwner;
+        li.id = 'js-' + removeWhitespace(uniqueTaskOwner);
     
         li.innerHTML =
             '<span class="board__taskOwner">' + uniqueTaskOwner + '</span>' +
@@ -70,61 +71,90 @@ function showTaskOwners () {
         liInProgress.id = liInProgressId;
         document.getElementById('board-container').appendChild(liInProgress);
 
-        var liDone = document.createElement('ul');
-        var liDoneClass = firstClass + ' done';
+        var liDone = document.createElement('li');
+        var liDoneClass = firstClass + ' ' + done;
         liDone.classList = liDoneClass;
         liDoneId = liDoneClass + i;
         liDone.id = liDoneId;
         document.getElementById('board-container').appendChild(liDone);
 
         var ulToDo = document.createElement('ul');
+        ulToDo.classList.add(toDo);
+        ulToDo.classList.add(removeWhitespace(uniqueTaskOwner));
         document.getElementById(liToDoId).appendChild(ulToDo);
-        
+
         var ulInProgress = document.createElement('ul');
+        ulInProgress.classList.add(toDo);
+        ulInProgress.classList.add(removeWhitespace(uniqueTaskOwner));
         document.getElementById(liInProgressId).appendChild(ulInProgress);
-        
+
         var ulDone = document.createElement('ul');
+        ulDone.classList.add(toDo);
+        ulDone.classList.add(removeWhitespace(uniqueTaskOwner));
         document.getElementById(liDoneId).appendChild(ulDone);
     });
-}    
-
-// function addListsToTaskOwnerRows() {
-//     uniqueTaskOwners.forEach(function () {
-        
-// }
-
-function createHtmlForTasks() {
-    var li = document.createElement('li');
-    var taskTileTaskId = task.taskId;
-    var taskTileTaskName = task.taskName;
-    li.className = 'js-taskTile';
-    li.id = 'js-task-' + taskTileTaskId;
-    
-    li.innerHTML = 
-        '<div>' +
-        '   <div class="task-priority"></div>' +
-        '   <a class="task-identifier">' +
-        '       <img src="" alt="" class="task-identifier-icon">' +
-        '       <span class="task-identifier-name">' + taskTileTaskId + '</span>' + 
-        '   </a>' +
-        '   <div><i class="fas fa-arrow-left"></i></div>' +
-        '   <span class="task-name">' + taskTileTaskName + '</span>' +
-        '   <img src="" alt="" class="taskOwner-avatar"' +
-        '</div>';
-    document.getElementById('board-container').appendChild(li);
-
 }
 
-// function placeTasksOnTheBoard () {
-//     tasks.forEach(function(task) {
-//         var taskColumn = task.column;
-//         switch (taskColumn) {
-//             case toDo: 
-            
-//         } 
+function removeWhitespace(str) {
+    return str.replace(' ', '');
+}
 
-//     }
-// }
+function createHtmlForTasks() {
+    uniqueTaskOwners.forEach(function (uniqueTaskOwner) {
+        filteredTasks.forEach(function (filteredTask) {        
+            if (filteredTask.taskOwnerName === uniqueTaskOwner) {
+                var li = document.createElement('li');
+                var taskTileTaskId = filteredTask.taskId;
+                var taskTileTaskName = filteredTask.taskName;
+                var taskClassName = removeWhitespace(uniqueTaskOwner);
+                var parentElementClassName = '.';
+                
+                switch (filteredTask.column) {
+                    case toDo:
+                        taskClassName += ' js-taskTile ' + toDo;
+                        parentElementClassName += toDo;
+                        li.className = taskClassName;
+                        break;
+                        
+                    case inProgress:
+                        taskClassName += ' js-taskTile ' + inProgress;
+                        parentElementClassName += inProgress;
+                        li.className = taskClassName;
+                        break;
+                            
+                    case done:
+                        taskClassName += ' js-taskTile ' + done;
+                        parentElementClassName += done;
+                        li.className = taskClassName;
+                        break;
+
+                    default:
+                        taskClassName += ' js-taskTile ' + toDo;
+                        parentElementClassName += toDo;
+                        li.className = taskClassName;
+                        break;
+                }
+
+                parentElementClassName += '.' + removeWhitespace(uniqueTaskOwner);
+                li.id = 'js-task-' + taskTileTaskId;
+                
+                li.innerHTML = 
+                    '<div style="display: flex;">' +
+                    '   <div class="task-priority"></div>' +
+                    '   <a class="task-identifier">' +
+                    '       <img src="" alt="" class="task-identifier-icon">' +
+                    '       <span class="task-identifier-name">' + taskTileTaskId + '</span>' + 
+                    '   </a>' +
+                    '   <div><i class="fas fa-arrow-left"></i></div>' +
+                    '   <span class="task-name">' + taskTileTaskName + '</span>' +
+                    '   <img src="" alt="" class="taskOwner-avatar"' +
+                    '</div>';
+
+                document.querySelector(parentElementClassName).appendChild(li);
+            }
+        });
+    });
+}
 
 document.getElementById('close-task-settings').addEventListener('click', function () {
     document.getElementById('bg-modal-taskEdit').style.display = 'none';
